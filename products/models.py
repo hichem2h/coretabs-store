d56ac5b54a465213221c82cf97acdfeb30ec958d
hichem2h <hichem2h@gmail.com>
2020-04-12 18:40:15 +0100

    Add created and updated for product model

--- products/models.py
+++ products/models.py
@@ -7,9 +7,10 @@ class Product(models.Model):
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
-
         # We can use reverse directly in the template
         return reverse('product_details', args=(self.id,))
 
