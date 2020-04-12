5b29513eb8e9663f38fdb27855f5d52685b47f92
hichem2h <hichem2h@gmail.com>
2020-04-12 22:25:30 +0100

    Add reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
 
     path('signup', signup, name='signup'),
     path('activate/<uid>/<token>', activate_email, name='activate_email'),
+
+    path('password-reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
