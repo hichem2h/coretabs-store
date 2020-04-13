047745df8c7317130f3df76920bc6b48dd889c34
hichem2h <hichem2h@gmail.com>
2020-04-13 19:09:45 +0100

    Update templates

--- coretabs_store/urls.py
+++ coretabs_store/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic.base import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('products.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('products/', include('products.urls')),
     path('accounts/', include('accounts.urls')),
     path('cart/', include('carts.urls')),
     path('orders/', include('orders.urls')),
--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from .views import product_list, product_details, add_product, edit_product
 
 
 urlpatterns = [
-    path('products/', product_list, name='product_list'),
-    path('products/new', add_product, name='add_product'),
-    path('products/<pk>', product_details, name='product_details'),
-    path('products/edit/<pk>', edit_product, name='edit_product'),
+    path('', product_list, name='product_list'),
+    path('new', add_product, name='add_product'),
+    path('<pk>', product_details, name='product_details'),
+    path('edit/<pk>', edit_product, name='edit_product'),
 ]
