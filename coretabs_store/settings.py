66f06a298e9119ecba3a4fb2a1e9c458081f8984
hichem2h <hichem2h@gmail.com>
2020-04-14 15:48:55 +0100

    Add serving media settings

--- coretabs_store/settings.py
+++ coretabs_store/settings.py
@@ -129,3 +129,6 @@ LOGOUT_REDIRECT_URL = 'product_list'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
