331e893ff419e96e5bd90e3f0e0e494f00ea4bba
YaserAlnajjar <kasseryasser@windowslive.com>
2020-04-25 15:09:03 +0300

    Uniform single quote usage

--- store/settings.py
+++ store/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_REDIRECT_URL = 'product_list'
