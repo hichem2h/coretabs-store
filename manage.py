48a6c5bb0c563f6aede99ecf5bbca8a8a7fd3ecd
YaserAlnajjar <kasseryasser@windowslive.com>
2020-04-25 15:07:21 +0300

    Change coretabs_store occurances into store

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coretabs_store.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
