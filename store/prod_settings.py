c9711ccb7d89cc13550f0fae6467b49094bd72a9
YaserAlnajjar <kasseryasser@windowslive.com>
2020-05-06 05:10:53 +0300

    Add sendgrid production settings

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -17,10 +17,16 @@ else:
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(';')
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
+
+# Mail Server
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
